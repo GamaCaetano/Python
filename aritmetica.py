1f4f9a0108bd958a1867797039fc6987b98e6c07
Jeison <87706961+GamaCaetano@users.noreply.github.com>
2021-11-13 18:12:15 -0300

    Update aritmetica.py

--- aritmetica.py
+++ aritmetica.py
@@ -12,8 +12,8 @@ def divisao():
 def potenciacao():
     return num1 ** num2
 
-print('A soma de',num1,'mais', num2, 'é:',soma())
-print('A subtracao de',num1,'menos',num2,'é:', subtracao())
-print('A multiplicação de',num1,'por',num2,'é igual a:',multiplicacao())
-print('A divisão de',num1,'por',num2,'é:',divisao())
-print('A potenciação de',num1,'elevado a',num2,'tem como resultado:',potenciacao())
+print('A soma de {} mais {} é: {}'.format(num1, num2, soma()))
+print('A subtracao de {} menos {} é: {}'.format(num1, num2, subtracao()))
+print('A multiplicação de {} por {} é igual a: {}'.format(num1, num2, multiplicacao()))
+print('A divisão de {} por {} é: {}'.format(num1, num2, divisao()))
+print('A potenciação de {} elevado a {} tem como resultado: {}'.format(num1, num2, potenciacao()))
